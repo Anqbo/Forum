dac7ff42db6d7605156be34d5be8452c70362283
user <user@Ubuntu.myguest.virtualbox.org>
2024-05-24 13:20:10 +0200

    9.0 add services

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -4,27 +4,42 @@ declare(strict_types=1);
 
 namespace App\Controller;
 use App\Repository\WpisRepository;
+use App\Service\PostProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ForumController extends AbstractController{
    
-    public function __construct(private WpisRepository $wpisRepository){
+    public function __construct(
+        private WpisRepository $wpisRepository,
+        private PostProvider $postProvider
+    ){
 
     } 
     
-    #[Route('/wpisy', name: 'wpisy')]
-    public function mainPage():Response{
-        $wpisy = $this->wpisRepository->findAll();
+    
+    // public function mainPage():Response{
+    //     $wpisy = $this->wpisRepository->findAll();
 
-        return $this->render('forum/wpisy.html.twig', ['wpisy' => $wpisy]);
-    }
+    //     return $this->render('forum/wpisy.html.twig', ['wpisy' => $wpisy]);
+    // }
 
     #[Route('/wpis/{id}', name: 'one_wpis')]
     public function getOneWpisById(int $id){
         $wpis = $this->wpisRepository->find($id);
 
         return $this->render('forum/wpis.html.twig', ['wpis' => $wpis]);
+    }
+
+    #[Route('/wpisy', name: 'wpisy')]
+    public function showPost(): Response{
+        $wpisy = $this->wpisRepository->findAll();
+        $parameters = [];
+        if ($wpisy) {
+            $parameters = $this->postProvider->transformDataForTwig($wpisy);
+        }
+
+        return $this->render('forum/wpisy.html.twig', $parameters);
     }
 }
---
+++ src/Service/PostProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+class PostProvider{
+    public function transformDataForTwig(array $wpisy): array{
+        $transformedData = [];
+        foreach($wpisy as $wpis){
+            $transformedData['wpisy'][] = [
+                'id' => $wpis->getId(),
+                'title' => $wpis->getTitle(),
+                'category' => $wpis->getCategory(),
+                'content' => substr($wpis->getContent(), 0, 30).'...',
+                'link' => 'wpis/'.$wpis->getId(),
+            ];
+        }
+
+        return $transformedData;
+    }
+}
